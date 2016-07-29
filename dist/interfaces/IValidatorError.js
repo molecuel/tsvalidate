daadc1410d2ce65158254f6e3260aa7c852137b1
DanielTWalther <daniel.walther@inspirationlabs.com>
2016-07-29 10:26:31 +0200

    Removed Dist

--- dist/interfaces/IValidatorError.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-
-//# sourceMappingURL=IValidatorError.js.map
--- dist/interfaces/IValidatorOptions.d.ts
+++
@@ -1,6 +0,0 @@
-export interface IValidatorOptions {
-    each?: boolean;
-    message?: string | ((value?: any, constraint1?: any, constraint2?: any) => string);
-    groups?: string[];
-    always?: boolean;
-}
